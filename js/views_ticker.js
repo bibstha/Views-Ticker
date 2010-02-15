de1a80f98e5b52b141958508dbdbea37f5c903cd
Bibek Shrestha <bibekshrestha@gmail.com>
2010-02-15 16:53:40 +0545

    Fixed views options to get speed

--- js/views_ticker.js
+++ js/views_ticker.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   if ($("#news-ticker").length > 0) {
-    $("#news-ticker").liScroll();
+    if (null == Drupal.settings.views_ticker.speed) {
+      speed = 0.07;
+    } else {
+      speed = Drupal.settings.views_ticker.speed;
+    }
+    $("#news-ticker").liScroll({travelocity: speed});
   }
 });
